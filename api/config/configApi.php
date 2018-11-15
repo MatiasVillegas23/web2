8a00ba493bfa84e045867724cd9305c1734b805b
Matias Villegas <matias.villegas23@gmail.com>
2018-11-15 19:56:31 -0300

    get prodcto/s api created

---
+++ api/config/configApi.php
@@ -0,0 +1,11 @@
+<?php
+class ConfigApi{
+  public static $RESOURCE = "resource";
+  public static $PARAMS = "params";
+  public static $RESOURCES = [
+    'index#GET' => 'productoApiController#index',
+
+      ];
+
+}
+?>
